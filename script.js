c3d2a8e516e1b6ee17a9d7592de1fcfe09a798fd
Emmanuel Canto Vazquez <emikogears134@gmail.com>
2023-09-14 16:11:47 -0600

    feat: Add routes.

--- script.js
+++ script.js
@@ -6,16 +6,40 @@ const puebla = require("./puebla.json");
   const locations = puebla;
   const { features } = locations;
 
-  const _locations =
-    features.slice(0, 10)
-      .map((feature) => ({ zipCode: feature.properties.d_cp, coordinates: feature.geometry.coordinates }))
-      .filter(({ coordinates }) => Array.isArray(coordinates)).map(({ zipCode, coordinates }) => ({ zipCode, coordinates: coordinates[0] })).reduce((acc, { zipCode, coordinates }) => ({ ...acc, [zipCode]: {
-        zipCode,
-        coordinates: coordinates.map(([longitude, latitude]) => [latitude, longitude])
-      } }), {});
+  const municipalities = features
+    .slice(0, 100)
+    .map((feature) => ({
+      zipCode: feature.properties.d_cp,
+      coordinates: feature.geometry.coordinates,
+    }))
+    .filter(({ coordinates }) => Array.isArray(coordinates))
+    .map(({ zipCode, coordinates }) => ({
+      zipCode,
+      coordinates: coordinates[0].map(([longitude, latitude]) => [
+        latitude,
+        longitude,
+      ]),
+    }));
+  // .reduce(
+  //   (acc, { zipCode, coordinates }) => ({
+  //     ...acc,
+  //     [zipCode]: {
+  //       zipCode,
+  // coordinates: coordinates.map(([longitude, latitude]) => [
+  //   latitude,
+  //   longitude,
+  // ]),
+  //     },
+  //   }),
+  //   {}
+  // );
+
+  const db = {
+    municipalities,
+  };
 
   fs.writeFileSync(
     path.join(__dirname, "puebla-cp.json"),
-    JSON.stringify(_locations, null, 2)
+    JSON.stringify(db, null, 2)
   );
 })();
